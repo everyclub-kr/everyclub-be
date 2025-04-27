dad68a1d88bc555196b4399eeae93a8ba60b2146
Hwan0518 <qip0518@naver.com>
2025-04-28 00:59:16 +0900

    init: generate 'EVERYCLUB' project

---
+++ src/test/java/everyclub/be/EveryclubBeApplicationTests.java
@@ -0,0 +1,15 @@
+package everyclub.be;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class EveryclubBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
